168c37a3dd38ba24b620acd37230d0e336391259
Naija H <naijahodigere11@gmail.com>
2025-04-15 03:05:06 +0000

    Finish mean variance std calculator project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-def calculate(list):
+def calculate(list1):
 
+    calculations=dict() #axis 1 - column wise, axis 2-row wise, flattened - total array
+    if len(list1) > 9 or len(list1) < 9 :
+        raise ValueError('List must contain nine numbers.')
+    else:
+        matrix=np.array(list1).reshape(3,3)
 
-
-
+        calculations={
+            'mean':[matrix.mean(axis=0).tolist(),matrix.mean(axis=1).tolist(),matrix.mean().item()],
+            'variance':[matrix.var(axis=0).tolist(),matrix.var(axis=1).tolist(),matrix.var().item()],
+            'standard deviation':[matrix.std(axis=0).tolist(),matrix.std(axis=1).tolist(),matrix.std().item()],
+            'max':[matrix.max(axis=0).tolist(),matrix.max(axis=1).tolist(),matrix.max().item()],
+            'min':[matrix.min(axis=0).tolist(),matrix.min(axis=1).tolist(),matrix.min().item()],
+            'sum':[matrix.sum(axis=0).tolist(),matrix.sum(axis=1).tolist(),matrix.sum().item()]
+            }
+            
     return calculations
